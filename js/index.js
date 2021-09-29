3e6c610aeab1828df95344f08c8bead566e8656c
Luciano Escalante <75239876+Badghel@users.noreply.github.com>
2021-09-29 16:41:40 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+    let ig = document.querySelector(".ig");
+    ig.addEventListener("click",()=>{
+        window.open("https://www.instagram.com/_burger1337/?hl=es");
+    })
+
+    let ig2 = document.querySelector(".ig2");
+    ig2.addEventListener("click",()=>{
+        window.open("https://www.instagram.com/_burger1337/?hl=es");
+    })
